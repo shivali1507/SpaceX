5f79f739438ca0c8d162e0aa2c15c77644dd707b
shivali1507 <139458170+shivali1507@users.noreply.github.com>
2024-04-15 04:59:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import Index from "./pages";
+import Index from "./pages/Index";
 
 export default function App() {
   return (
